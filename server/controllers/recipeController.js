0c5aff4953f5cc08b344c91658c4da5bc59b6836
Philip Blunk <philipblunk@gmail.com>
2023-12-17 09:36:25 -0600

    recipe blog

---
+++ server/controllers/recipeController.js
@@ -0,0 +1,282 @@
+require('../models/database');
+const Category = require('../models/Category');
+const Recipe = require('../models/Recipe');
+
+/**
+ *  GET /
+ * Homepage
+ */
+exports.homepage = async (req, res) => {
+  try {
+    const limitNumber = 5;
+    const categories = await Category.find({}).limit(limitNumber);
+    const latest = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    const thai = await Recipe.find({ category: 'Thai' }).limit(limitNumber);
+    const american = await Recipe.find({ category: 'American' }).limit(
+      limitNumber
+    );
+    const chinese = await Recipe.find({ category: 'Chinese' }).limit(
+      limitNumber
+    );
+
+    const food = { latest, thai, american, chinese };
+
+    res.render('index', { title: 'Recipe Blog - Home', categories, food });
+  } catch (error) {
+    res.status(500).send({ message: error.message || 'Error Occured' });
+  }
+};
+
+/**
+ *  GET /categories
+ * Categories
+ */
+exports.exploreCategories = async (req, res) => {
+  try {
+    const limitNumber = 20;
+    const categories = await Category.find({}).limit(limitNumber);
+
+    res.render('categories', { title: 'Recipe Blog - Categories', categories });
+  } catch (error) {
+    res.status(500).send({ message: error.message || 'Error Occured' });
+  }
+};
+
+/**
+ *  GET /categories:/id
+ * Categories By Id
+ */
+exports.exploreCategoriesById = async (req, res) => {
+  try {
+    let categoryId = req.params.id;
+    const limitNumber = 20;
+    const categoryById = await Recipe.find({ category: categoryId }).limit(
+      limitNumber
+    );
+    res.render('categories', {
+      title: 'Recipe Blog - Categories',
+      categoryById,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message || 'Error Occured' });
+  }
+};
+
+/**
+ *  GET /recipe:id
+ * Recipe
+ */
+exports.exploreRecipe = async (req, res) => {
+  try {
+    let recipeId = req.params.id;
+    const recipe = await Recipe.findById(recipeId);
+    res.render('recipe', { title: 'Recipe Blog - Recipe', recipe });
+  } catch (error) {
+    res.status(500).send({ message: error.message || 'Error Occured' });
+  }
+};
+
+/**
+ *  POST /search
+ * Search
+ */
+
+exports.searchRecipe = async (req, res) => {
+  try {
+    let searchTerm = req.body.searchTerm;
+    let recipe = await Recipe.find({
+      $text: { $search: searchTerm, $diacriticSensitive: true },
+    });
+
+    res.render('search', { title: 'Recipe Blog - Search', recipe });
+  } catch (error) {
+    res.status(500).send({ message: error.message || 'Error Occured' });
+  }
+};
+
+/**
+ *  GET /explore-latest
+ * Explore Latest
+ */
+exports.exploreLatest = async (req, res) => {
+  try {
+    const limitNumber = 20;
+    const recipe = await Recipe.find({}).sort({ _id: -1 }).limit(limitNumber);
+    res.render('explore-latest', {
+      title: 'Recipe Blog - Explore Latest',
+      recipe,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message || 'Error Occured' });
+  }
+};
+
+/**
+ *  GET /explore-random
+ * Explore Random
+ */
+exports.exploreRandom = async (req, res) => {
+  try {
+    let count = await Recipe.find().countDocuments();
+    let random = Math.floor(Math.random() * count);
+    let recipe = await Recipe.findOne().skip(random).exec();
+    res.render('explore-random', {
+      title: 'Recipe Blog - Explore Random',
+      recipe,
+    });
+  } catch (error) {
+    res.status(500).send({ message: error.message || 'Error Occured' });
+  }
+};
+
+/**
+ *  GET /submit-recipe
+ * Submit Recipe
+ */
+exports.submitRecipe = async (req, res) => {
+  const infoErrorsObj = req.flash('infoErrors');
+  const infoSubmitObj = req.flash('infoSubmit');
+  res.render('submit-recipe', {
+    title: 'Recipe Blog - Submit Recipe',
+    infoErrorsObj,
+    infoSubmitObj,
+  });
+};
+
+/**
+ *  POST /submit-recipe
+ * Submit Recipe
+ */
+exports.submitRecipeOnPost = async (req, res) => {
+  try {
+    let imageUploadFile;
+    let uploadPath;
+    let newImageName;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+      console.log('No files were uploaded.');
+    } else {
+      imageUploadFile = req.files.image;
+      newImageName = Date.now() + imageUploadFile.name;
+
+      uploadPath =
+        require('path').resolve('./') + '/public/uploads/' + newImageName;
+
+      imageUploadFile.mv(uploadPath, function (err) {
+        if (err) return res.status(500).send(err);
+      });
+    }
+
+    const newRecipe = new Recipe({
+      name: req.body.name,
+      description: req.body.description,
+      email: req.body.email,
+      ingredients: req.body.ingredients,
+      category: req.body.category,
+      image: newImageName,
+    });
+    await newRecipe.save();
+
+    req.flash('infoSubmit', 'Recipe has been added.');
+    res.redirect('/submit-recipe');
+  } catch (error) {
+    //res.json(error);
+    req.flash('infoErrors', error);
+    res.redirect('/submit-recipe');
+  }
+};
+
+async function deleteRecipe() {
+  try {
+    await Recipe.deleteOne({ name: 'Orange Chicken' });
+  } catch (error) {
+    console.log(error);
+  }
+}
+deleteRecipe();
+
+async function updateRecipe() {
+  try {
+    const res = await Recipe.updateOne(
+      { name: 'New Chocolate Cake' },
+      { name: 'New Recipe' }
+    );
+    res.n; // Number of documents matched
+    res.nModified; // Number of documents modified
+  } catch (error) {
+    console.log(error);
+  }
+}
+updateRecipe();
+
+/*async function insertDummyCategoryData() {
+  try {
+    await Category.insertMany([
+      {
+        name: 'Thai',
+        image: 'thai-food.jpg',
+      },
+      {
+        name: 'American',
+        image: 'american-food.jpg',
+      },
+      {
+        name: 'Chinese',
+        image: 'chinese-food.jpg',
+      },
+      {
+        name: 'Mexican',
+        image: 'mexican-food.jpg',
+      },
+      {
+        name: 'Indian',
+        image: 'indian-food.jpg',
+      },
+      {
+        name: 'Spanish',
+        image: 'spanish-food.jpg',
+      },
+    ]);
+  } catch (error) {
+    console.log('err', +error);
+  }
+}
+
+insertDummyCategoryData(); 
+
+async function insertDummyRecipeData() {
+  try {
+    await Recipe.insertMany([
+      {
+        name: 'Recipe Name Goes Here',
+        description: 'Recipe Description Goes Here',
+        email: 'recipeemail@test.com',
+        ingredients: [
+          '1 teaspoon baking powder',
+          '1 teaspoon cayenne pepper',
+          '1 cup milk',
+          '1 teaspoon smoked paprika',
+        ],
+        category: 'American',
+        image: 'southern-fried-chicken.jpg',
+      },
+      {
+        name: 'Recipe Name Goes Here',
+        description: 'Recipe Description Goes Here',
+        email: 'recipeemail@test.com',
+        ingredients: [
+          '1 teaspoon baking powder',
+          '1 teaspoon cayenne pepper',
+          '1 cup milk',
+          '1 teaspoon smoked paprika',
+        ],
+        category: 'American',
+        image: 'southern-fried-chicken.jpg',
+      },
+    ]);
+  } catch (error) {
+    console.log('err', +error);
+  }
+}
+
+insertDummyRecipeData();*/
